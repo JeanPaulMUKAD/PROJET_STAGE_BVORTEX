25bfe243c1352568a9a71055d0a014dc4823be51
Fabrice-sangwa <19sm476@esisalama.org>
2024-03-05 16:49:28 +0200

    feat : init expert solution module

---
+++ expert_solution/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "expert_solution",
+
+    'summary': "Expert soluition for accounting TVA",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "bvortex",
+    'website': "https://www.yourcompany.com",
+
+
+    'category': 'Uncategorized',
+    'version': '17.0',
+
+    'depends': ['base', 'sale_management'],
+
+    'data': [
+        'views/expert_solution_views.xml',
+    ],
+    'application': True,
+
+}
+
---
+++ expert_solution/controllers/controllers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class ../bvortexCommercial/customSales(http.Controller):
+#     @http.route('/../bvortex_commercial/expert_solution/../bvortex_commercial/expert_solution', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/../bvortex_commercial/expert_solution/../bvortex_commercial/expert_solution/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('../bvortex_commercial/expert_solution.listing', {
+#             'root': '/../bvortex_commercial/expert_solution/../bvortex_commercial/expert_solution',
+#             'objects': http.request.env['../bvortex_commercial/expert_solution.../bvortex_commercial/expert_solution'].search([]),
+#         })
+
+#     @http.route('/../bvortex_commercial/expert_solution/../bvortex_commercial/expert_solution/objects/<model("../bvortex_commercial/expert_solution.../bvortex_commercial/expert_solution"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('../bvortex_commercial/expert_solution.object', {
+#             'object': obj
+#         })
+
